138375255cf6b367aee10933b079058bb6c7d0a3
VinayGitHub0481 <madagonivinay@gmail.com>
2024-12-15 11:08:35 +0530

    Update AdminImp.java

--- src/main/java/com/DAO/AdminImp.java
+++ src/main/java/com/DAO/AdminImp.java
@@ -6,12 +6,12 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.admin.Admin;
-@Repository
+@Repository          
 public class AdminImp {
     @Autowired
-	private HibernateTemplate hibernateTemplate;
+   private HibernateTemplate hibernateTemplate;
 
-  @Transactional(readOnly = false)
+  @Transactional(readOnly = false)  //here by making transactional(readOnly =false) by this we can perform insert Operations i.e insert,update,delete Operations.
 	public int save(Admin ad) {
 		int id=(int)this.hibernateTemplate.save(ad);
 		return id;
